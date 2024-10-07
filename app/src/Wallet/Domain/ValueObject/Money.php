5fc5da72f1ec1574dceaf969d0b1883e42314f35
Carlos Marcelo <carlos.marcelo1992@gmail.com>
2024-10-07 10:16:10 +0200

    add restriction about money values

--- app/src/Wallet/Domain/ValueObject/Money.php
+++ app/src/Wallet/Domain/ValueObject/Money.php
@@ -6,10 +6,16 @@ use App\Wallet\Domain\Exception\InvalidMoneyAmountException;
 
 class Money
 {
+    private const ACCEPTED_VALUES = [0.05, 0.10, 0.25, 1.00];
+
     private float $amount;
 
     public function __construct(float $amount)
     {
+        if (!in_array($amount, self::ACCEPTED_VALUES, true)) {
+            throw new InvalidMoneyAmountException('Invalid money amount. Accepted values are 0.05, 0.10, 0.25, and 1.');
+        }
+
         if ($amount < 0) {
             throw new InvalidMoneyAmountException('Amount cannot be negative.');
         }
